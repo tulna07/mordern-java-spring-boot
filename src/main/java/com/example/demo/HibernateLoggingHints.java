2c3f9bd79721f1cc9f95bbe73630b74d84b6ec44
tulna <tu.cloud07@gmail.com>
2026-03-12 14:34:26 +0700

    fix: register complete set of Hibernate jboss-logging _$logger classes

--- src/main/java/com/example/demo/HibernateLoggingHints.java
+++ src/main/java/com/example/demo/HibernateLoggingHints.java
@@ -15,15 +15,28 @@ public class HibernateLoggingHints {
         private static final String[] LOGGER_IMPLS = {
             "org.hibernate.jpa.internal.JpaLogger_$logger",
             "org.hibernate.boot.BootLogging_$logger",
+            "org.hibernate.boot.archive.scan.internal.ScannerLogger_$logger",
+            "org.hibernate.boot.jaxb.JaxbLogger_$logger",
+            "org.hibernate.bytecode.enhance.spi.interceptor.BytecodeInterceptorLogging_$logger",
+            "org.hibernate.cache.spi.SecondLevelCacheLogger_$logger",
+            "org.hibernate.dialect.DialectLogging_$logger",
+            "org.hibernate.engine.jdbc.JdbcLogging_$logger",
+            "org.hibernate.engine.jdbc.batch.JdbcBatchLogging_$logger",
+            "org.hibernate.engine.jdbc.env.internal.LobCreationLogging_$logger",
+            "org.hibernate.engine.jdbc.spi.SQLExceptionLogging_$logger",
+            "org.hibernate.id.enhanced.TableGeneratorLogger_$logger",
             "org.hibernate.internal.CoreMessageLogger_$logger",
-            "org.hibernate.service.internal.ServiceLogger_$logger",
+            "org.hibernate.internal.SessionFactoryRegistryMessageLogger_$logger",
+            "org.hibernate.internal.log.ConnectionInfoLogger_$logger",
             "org.hibernate.internal.log.DeprecationLogger_$logger",
-            "org.hibernate.orm.connections.pooling.ConnectionPoolingLogger_$logger",
-            "org.hibernate.orm.jdbc.bind.BindingLogger_$logger",
-            "org.hibernate.orm.model.mapping.MappingModelLogger_$logger",
-            "org.hibernate.orm.query.QueryLogger_$logger",
-            "org.hibernate.orm.schema.SchemaLogger_$logger",
-            "org.hibernate.orm.transaction.TransactionLogger_$logger"
+            "org.hibernate.internal.log.IncubationLogger_$logger",
+            "org.hibernate.internal.log.UrlMessageBundle_$logger",
+            "org.hibernate.query.QueryLogging_$logger",
+            "org.hibernate.resource.beans.internal.BeansMessageLogger_$logger",
+            "org.hibernate.resource.jdbc.internal.ResourceRegistryLogger_$logger",
+            "org.hibernate.resource.jdbc.internal.LogicalConnectionLogging_$logger",
+            "org.hibernate.service.internal.ServiceLogger_$logger",
+            "org.hibernate.validator.internal.util.logging.Log_$logger"
         };
 
         @Override
