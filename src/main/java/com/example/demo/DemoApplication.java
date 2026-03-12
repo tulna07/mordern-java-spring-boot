070e66a2dab1dcf7074dc45df9caf5bab51b196d
tulna <tu.cloud07@gmail.com>
2026-03-12 13:59:35 +0700

    fix: use GraalVM tracing agent in Dockerfile to auto-generate reflection metadata

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.setProperty("org.jboss.logging.provider", "slf4j");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
