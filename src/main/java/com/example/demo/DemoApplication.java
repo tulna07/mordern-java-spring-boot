194dac7564b6351718ac968562022661eedcc29e
tulna <tu.cloud07@gmail.com>
2026-03-13 09:36:07 +0700

    fix: set hibernate.bytecode.provider=none as system property before context init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.setProperty("hibernate.bytecode.provider", "none");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
